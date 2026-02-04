05abbc8d5a52bf82a65792262df6277d6013cba3
Lou Böcker <lou@boecker.dev>
2026-02-04 01:23:39 +0100

    river: add tailscale client

--- hosts/river/networking.nix
+++ hosts/river/networking.nix
@@ -1,4 +1,9 @@
 {...}: {
   networking.networkmanager.enable = true;
   networking.firewall.enable = false;
+
+  services.tailscale = {
+    enable = true;
+    useRoutingFeatures = "client";
+  };
 }
